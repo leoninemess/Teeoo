dee6e3f015f2c430eac2d4489073178fbc3b4077
iatw <1982890538@qq.com>
2018-04-14 18:03:51 +0800

    markdown

--- public/themes/snow/layouts/layout.blade.php
+++ public/themes/snow/layouts/layout.blade.php
@@ -74,12 +74,7 @@
                 })
             });
             editormd.markdownToHTML("test-editormd-view2", {
-                htmlDecode      : "style,script,iframe",  // you can filter tags decode
-                emoji           : true,
-                taskList        : true,
-                tex             : true,  // 默认不解析
-                flowChart       : true,  // 默认不解析
-                sequenceDiagram : true,  // 默认不解析
+
             });
         });
 
